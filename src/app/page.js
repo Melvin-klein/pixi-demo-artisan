ac527dbbaafde3042630564cf3196a111feffdf5
Melvine Nargeot <melvine.nargeot@gmail.com>
2023-11-04 12:17:37 +0100

    Fix demo artisan

--- src/app/page.js
+++ src/app/page.js
@@ -252,12 +252,12 @@ export default function Home() {
               <div className="fixed inset-0 z-50" />
               <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10">
                 <div className="flex items-center justify-between">
-                  <a href="#" className="flex items-center -m-1.5 p-1.5">
+                  <a href="#" className="flex items-center -m-1.5 p-1.5 text-white">
                       <span className="sr-only">Demo Artisan</span>
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-white">
                           <path strokeLinecap="round" strokeLinejoin="round" d="M11.42 15.17L17.25 21A2.652 2.652 0 0021 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 11-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 004.486-6.336l-3.276 3.277a3.004 3.004 0 01-2.25-2.25l3.276-3.276a4.5 4.5 0 00-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437l1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008z" />
                       </svg>
-                      <span className="ml-2 font-bold text-lead">Demo Artisan</span>
+                      <span className="ml-2 font-bold text-lead text-white">Demo Artisan</span>
                   </a>
                   <button
                     type="button"
@@ -584,7 +584,7 @@ export default function Home() {
                 </div>
                 <div className="mt-8 flex justify-end">
                   <button
-                    type="submit"
+                    type="button"
                     className="rounded-md bg-yellow-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-yellow-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-600"
                     onClick={() => setShowModal(true)}
                   >
